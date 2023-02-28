524df8d02c04a0c88fd2e6b530f6fffc85264f0e
Olumorothy <iseoluwa89@gmail.com>
2023-02-28 22:29:51 +0000

    implements the registration functionality

--- client/src/components/Register.jsx
+++ client/src/components/Register.jsx
@@ -1,5 +1,66 @@
 import React from "react";
+import { useState } from "react";
+import { Link } from "react-router-dom";
 
 export default function Register() {
-  return <div>Register page</div>;
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [email, setEmail] = useState("");
+
+  const handleRegistration = (e) => {
+    e.preventDefault();
+    console.log(username, password, confirmPassword, email);
+    setUsername("");
+    setPassword("");
+    setConfirmPassword("");
+    setEmail("");
+  };
+  return (
+    <main className="register">
+      <h1 className="registerTitle">Create an account</h1>
+      <form className="registerForm" onSubmit={handleRegistration}>
+        <label htmlFor="username">Username</label>
+        <input
+          type="text"
+          name="username"
+          id="username"
+          required
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <label htmlFor="email">Email Address</label>
+        <input
+          type="text"
+          name="email"
+          id="email"
+          required
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <label htmlFor="password">Password</label>
+        <input
+          type="password"
+          name="password"
+          id="password"
+          required
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <label htmlFor="password">Confirm Password</label>
+        <input
+          type="password"
+          name="cpassword"
+          id="cpassword"
+          required
+          value={confirmPassword}
+          onChange={(e) => setConfirmPassword(e.target.value)}
+        />
+        <button className="registerBtn">REGISTER</button>
+        <p>
+          Have an account? <Link to="/">Sign in</Link>
+        </p>
+      </form>
+    </main>
+  );
 }
