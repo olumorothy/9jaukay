626bb9651199a8bdeb1da0992b66226ccbd91003
Olumorothy <iseoluwa89@gmail.com>
2023-02-28 22:03:55 +0000

    adds functionality for login

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,15 +1,39 @@
 import React from "react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    console.log({ email, password });
+    setEmail("");
+    setPassword("");
+  };
   return (
     <main className="login">
       <h1 className="loginTitle">Log into your account</h1>
-      <form className="loginForm">
+      <form className="loginForm" onSubmit={handleLogin}>
         <label htmlFor="email">Email Address</label>
-        <input type="text" name="email" id="email" required />
+        <input
+          type="text"
+          name="email"
+          id="email"
+          required
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
         <label htmlFor="password">Password</label>
-        <input type="password" name="password" id="password" required />
+        <input
+          type="password"
+          name="password"
+          id="password"
+          required
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
         <button className="loginBtn">SIGN IN</button>
         <p>
           Don't have an account? <Link to="/register">Create one</Link>
