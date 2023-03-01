e19406b52d52e3d5f28d9af3bdf4dadc44c7fc84
Olumorothy <iseoluwa89@gmail.com>
2023-03-01 00:14:23 +0000

    adds a post route to accepts users credentials

--- server/index.js
+++ server/index.js
@@ -7,8 +7,18 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
 
+const users = [];
+const generateID = () => Math.random().toString(36).substring(2, 10);
+
 app.get("/api", (req, res) => {
   res.json({ message: "Hello world" });
+});
+
+app.post("/api/register", async (req, res) => {
+  const { email, password, username } = req.body;
+
+  const id = generateID();
+  console.log({ email, password, username, id });
 });
 
 app.listen(PORT, () => {
