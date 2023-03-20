e407b37ccf21cc4a37af8f2ab15e89d840e9608a
Olumorothy <iseoluwa89@gmail.com>
2023-03-20 12:58:47 +0000

    adds functionality for replying to a comment

--- server/index.js
+++ server/index.js
@@ -64,13 +64,38 @@ app.post("/api/thread/like", (req, res) => {
 
   const threadLikes = result[0].likes;
 
-  const authenticateReaction = threadLikes.filter((user) => user.id === userId);
+  const authenticateReaction = threadLikes.filter((user) => user === userId);
 
   if (authenticateReaction.length === 0) {
     threadLikes.push(userId);
+
     return res.json({ message: "You've reacted to this post" });
   }
   res.json({ error_message: "You can only react once!" });
+});
+
+//displaying replies on each post
+app.post("/api/thread/replies", (req, res) => {
+  const { id } = req.body;
+
+  const result = threadList.filter((thread) => thread.id === id);
+
+  res.json({ replies: result[0].replies, title: result[0].title });
+});
+
+//post reply functionality
+app.post("/api/create/reply", async (req, res) => {
+  const { id, userId, reply } = req.body;
+  const result = threadList.filter((thread) => thread.id === id);
+  const user = users.filter((user) => user.id === userId);
+  console.log(user);
+  console.log(result[0]);
+  result[0].replies.unshift({
+    userId: user[0].id,
+    name: user[0].username,
+    text: reply,
+  });
+  res.json({ message: "Response added successfully!" });
 });
 
 app.post("/api/create/thread", async (req, res) => {
